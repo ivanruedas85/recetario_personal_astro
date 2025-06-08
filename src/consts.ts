bcd3012df5a9f9be4570d3ce0822e2b889c7ef09
ivanruedas85 <ivanruedas85@gmail.com>
2025-06-08 10:42:39 -0600

    Se actualizaron las variables

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
-export const SITE_TITLE = ""
-export const SITE_DESCRIPTION = ""
+export const SITE_TITLE = "Recetario familiar"
+export const SITE_DESCRIPTION = "Este es un recetario de la familia Santos Gonzalez"
+export const AUTHOR_NAME = "Teresa Gonzalez Torres"
