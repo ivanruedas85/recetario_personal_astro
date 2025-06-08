6e8fe699f6c07e7a1f8c5102807ecaedcf14dd9d
ivanruedas85 <ivanruedas85@gmail.com>
2025-06-08 12:13:45 -0600

    Se cambio el contenido  y la extención de json a js

---
+++ src/data/categorias.js
@@ -0,0 +1,58 @@
+export const categorias = [
+    {
+        id: 1,
+        name: "Comidas",
+        slug: "comidas",
+        count: 42,
+        image: "https://images.pexels.com/photos/1487511/pexels-photo-1487511.jpeg?auto=compress&cs=tinysrgb&w=500"
+    },
+    {
+        id: 2,
+        name: "Postres",
+        slug: "postres",
+        count: 36,
+        image: "https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=500"
+    },
+    {
+        id: 3,
+        name: "Desayunos",
+        slug: "desayunos",
+        count: 24,
+        image: "https://images.pexels.com/photos/414555/pexels-photo-414555.jpeg?auto=compress&cs=tinysrgb&w=500"
+    },
+    {
+        id: 4,
+        name: "Aperitivos",
+        slug: "aperitivos",
+        count: 18,
+        image: "https://images.pexels.com/photos/6941018/pexels-photo-6941018.jpeg?auto=compress&cs=tinysrgb&w=500"
+    },
+    {
+        id: 5,
+        name: "Sopas",
+        slug: "sopas",
+        count: 15,
+        image: "https://images.pexels.com/photos/539451/pexels-photo-539451.jpeg?auto=compress&cs=tinysrgb&w=500"
+    },
+    {
+        id: 6,
+        name: "Tapas",
+        slug: "tapas",
+        count: 22,
+        image: "https://images.pexels.com/photos/5175647/pexels-photo-5175647.jpeg?auto=compress&cs=tinysrgb&w=500"
+    },
+    {
+        id: 7,
+        name: "Bebidas",
+        slug: "bebidas",
+        count: 19,
+        image: "https://images.pexels.com/photos/2531187/pexels-photo-2531187.jpeg?auto=compress&cs=tinysrgb&w=500"
+    },
+    {
+        id: 8,
+        name: "Ensaladas",
+        slug: "ensaladas",
+        count: 17,
+        image: "https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&w=500"
+    }
+];
