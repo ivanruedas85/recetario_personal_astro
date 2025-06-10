6f7bf8d17b3d71b8556586977b6b6540578c5273
ivanruedas85 <ivanruedas85@gmail.com>
2025-06-10 01:14:49 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://ivanruedas85.github.io",
-    base: "recetario_personal_astro"
+    base: "recetario_personal_astro",
 });
